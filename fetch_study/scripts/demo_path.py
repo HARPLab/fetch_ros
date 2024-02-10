0428d90b1eaa9734e8beb8b4d2453848e83f288f
Ada Taylor <ada@idu.com>
2024-02-10 16:36:37 -0500

    Revert to fetch_nav and import it to demo points launch and script package

--- fetch_study/scripts/demo_path.py
+++ fetch_study/scripts/demo_path.py
@@ -67,32 +67,32 @@ class MoveBaseClient(object):
         self.client.send_goal(move_goal)
         self.client.wait_for_result()
 
-# Send a trajectory to controller
-class FollowTrajectoryClient(object):
+# # Send a trajectory to controller
+# class FollowTrajectoryClient(object):
 
-    def __init__(self, name, joint_names):
-        self.client = actionlib.SimpleActionClient("%s/follow_joint_trajectory" % name,
-                                                   FollowJointTrajectoryAction)
-        rospy.loginfo("Waiting for %s..." % name)
-        self.client.wait_for_server()
-        self.joint_names = joint_names
-
-    def move_to(self, positions, duration=5.0):
-        if len(self.joint_names) != len(positions):
-            print("Invalid trajectory position")
-            return False
-        trajectory = JointTrajectory()
-        trajectory.joint_names = self.joint_names
-        trajectory.points.append(JointTrajectoryPoint())
-        trajectory.points[0].positions = positions
-        trajectory.points[0].velocities = [0.0 for _ in positions]
-        trajectory.points[0].accelerations = [0.0 for _ in positions]
-        trajectory.points[0].time_from_start = rospy.Duration(duration)
-        follow_goal = FollowJointTrajectoryGoal()
-        follow_goal.trajectory = trajectory
-
-        self.client.send_goal(follow_goal)
-        self.client.wait_for_result()
+#     def __init__(self, name, joint_names):
+#         self.client = actionlib.SimpleActionClient("%s/follow_joint_trajectory" % name,
+#                                                    FollowJointTrajectoryAction)
+#         rospy.loginfo("Waiting for %s..." % name)
+#         self.client.wait_for_server()
+#         self.joint_names = joint_names
+# 
+#     def move_to(self, positions, duration=5.0):
+#         if len(self.joint_names) != len(positions):
+#             print("Invalid trajectory position")
+#             return False
+#         trajectory = JointTrajectory()
+#         trajectory.joint_names = self.joint_names
+#         trajectory.points.append(JointTrajectoryPoint())
+#         trajectory.points[0].positions = positions
+#         trajectory.points[0].velocities = [0.0 for _ in positions]
+#         trajectory.points[0].accelerations = [0.0 for _ in positions]
+#         trajectory.points[0].time_from_start = rospy.Duration(duration)
+#         follow_goal = FollowJointTrajectoryGoal()
+#         follow_goal.trajectory = trajectory
+# 
+#         self.client.send_goal(follow_goal)
+#         self.client.wait_for_result()
 
 
 if __name__ == "__main__":
